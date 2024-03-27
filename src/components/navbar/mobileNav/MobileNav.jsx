a6fb67f81d41a88a31eccf772131620f11ddb483
TRFLX2024 <sistemas@teraflex.cl>
2024-03-27 17:29:56 -0300

    build1

--- src/components/navbar/mobileNav/MobileNav.jsx
+++ src/components/navbar/mobileNav/MobileNav.jsx
@@ -10,10 +10,10 @@ const MobileNav = ({ isOpne, toggleMenu }) => {
                     <ul>
                         <Link className='decor' to={'/'}><li className='li-menu'>Inicio</li></Link>
                         <Link className='decor' to={'/quienes'}> <li className='li-menu'>Quiénes somos</li></Link>
-                        <li className='li-menu'>Contacto</li>
-                        <li className='li-menu'>Soporte</li>
-                        <li className='li-menu'>Sistema de pórticos</li>
-                        <li className='li-menu'>Revisa tus cámaras</li>
+                        <Link className='decor' to={'/contacto'}> <li className='li-menu'>Contacto</li></Link> 
+                        <a className='decor' href="https://soporte.montepalermo.cl/"><li className='li-menu'>Soporte</li></a> 
+                        <a className='decor' href="#"><li className='li-menu'>Sistema de pórticos</li></a>
+                        <a className='decor' href="https://isa.hik-connect.com/views/login/index.html?returnUrl=http://isa.hik-connect.com/devices/page&r=34655659560261127&host=isa.hik-connect.com&from=c17392dc2e6c405a931b#/main/overview"><li className='li-menu'>Revisa tus cámaras</li></a>
                     </ul>
                 </div>
             </div>
